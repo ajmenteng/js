1661ac0bf98102480694909c5988cb6d32f994a0
John Wesly <aj_menteng@yahoo.com>
2015-12-29 00:07:49 -0500

    modified desc in RomanNumeralConversion.js

--- RomanNumeralConversion.js
+++ RomanNumeralConversion.js
@@ -1,80 +1,108 @@
-//Collection of Roman number
-var roman={
-	'I':1,
-	'V':5,
-	'X':10,
-	'L':50,
-	'C':100,
-	'D':500,
-	'M':1000
-}
+/*
+ROMAN NUMERAL CONVERTER
+AUHTOR: JOHN CHANDRA (john@johnwesly.net)
 
-var digit={
-	1:'I',
-	5:'V',
-	10:'X',
-	50:'L',
-	100:'C',
-	500:'D',
-	1000:'M'
-}
+THIS SCRIPT IS TO CONVERT ROMAN NUMBER INTO ARABIC NUMBER VICE VERCA.
+*/
 
-var specialDigit={
-	4:'IV',
-	9:'IX',
-	40:'XL',
-	900:'CM'
-}
+/* 
+A LIST OF ROMAN NUMBER USED IN
+CONVERTING ROMAN NUMBER INTO ARABIC NUMBER
+*/
+var roman = {
+	'I' : 1,
+	'V' : 5,
+	'X' : 10,
+	'L' : 50,
+	'C' : 100,
+	'D' : 500,
+	'M' : 1000
+};
 
-var digitClass=[1000,100,10,1];
+/*
+A LIST OF ARABIC NUMBER USED IN
+CONVERTING ARABIC NUMBER INTO ROMAN NUMBER
+*/
+var digit = {
+	1 : 'I',
+	5 : 'V',
+	10 : 'X',
+	50 : 'L',
+	100 : 'C',
+	500 : 'D',
+	1000 : 'M'
+};
 
-//A function to convert Roman number to digit
-function roman2digit(n){
-	var ch=n.split("");
-	var currVal=0, result=0;
-	for(i=0;i<ch.length;i++){
-		//Check the next char
-		if(i+1<ch.length){
-			//Compare the current value of a roman number with the next one
-			//If the next one is greater, then subtract the current value
-			if(roman[ch[i]]<roman[ch[i+1]]){
-				result-=roman[ch[i]];
+/*
+A SPECIAL RULES APPLIED IN THE CONVERSION
+*/
+var specialDigit = {
+	4 : 'IV',
+	9 : 'IX',
+	40 : 'XL',
+	900 : 'CM'
+};
+
+var digitClass = [1000, 100, 10, 1];
+
+/*
+A FUNCTION FOR CONVERTING ROMAN TO ARABIC
+*/
+function roman2digit(n) {
+	var ch = n.split(""), currVal = 0, result = 0, i;
+	
+	/* ITERATE THROUGH EACH CHARACTER */
+	for (i = 0; i < ch.length; i + 1) {
+		
+		/* CHECK THE NEXT CHARACTER */
+		if (i + 1 < ch.length) {
+			
+			/*
+			CHECK IF THE NEXT CHARACTER IS GREATER.
+			IF SO, THEN SUBTRACT WITH THE CURRENT CHARACTER'S VALUE.
+			THIS IS FOR SPECIAL CASE LIKE 'IV', 'IX'.
+			OTHERWISE, ADD THE CURRENT VALUE TO THE RESULT CALCULATION.
+			*/
+			if (roman[ch[i]] < roman[ch[i + 1]]) {
+				result -= roman[ch[i]];
+			} else {
+				result += roman[ch[i]];
 			}
-			//otherwise, add it up
-			else{
-				result+=roman[ch[i]];
-			}			
-		}
-		else{
-			result+=roman[ch[i]];
+		} else {
+			result += roman[ch[i]];
 		}
 	}
 	return result;
 }
 
-function digit2roman(n){
-	var currVal=n, num, result="";	
-	for(i=0;i<digitClass.length;i++){
+/*
+A FUNCTION FOR CONVERTING ARABIC TO ROMAN
+*/
+function digit2roman(n) {
+	var currVal = n, num, result = "", i, j;
+	for (i = 0; i < digitClass.length; i + 1) {
 
-		if(currVal>digitClass[i]){
-
-			//Check if special number exist: 4:IV, 9:IX, etc
-			//If exists then return the value		
-			if(specialDigit[currVal]){
-				result=result+specialDigit[currVal];
+		if (currVal > digitClass[i]) {
+			/*
+			CHECK IF SPECIAL NUMBER EXIST:
+			4: IV, 9: IX, ETC
+			IF SO, THEN RETURN THE VALUE
+			*/
+			if (specialDigit[currVal]) {
+				result = result + specialDigit[currVal];
 				return result;
 			}
+			
+			/* CALCULATE THE VALUE OF ROMAN NUMBER */
+			num = Math.floor(currVal / digitClass[i]);
 
-			//Calculate the number of Roman number
-			num=Math.floor(currVal/digitClass[i]);
-
-			//Add the roman number as many as required
-			for(j=0;j<num;j++){
-				result=result+digit[digitClass[i]];
+			/* ADD THE ROMAN NUMBER AS MANY AS REQUIRED */
+			for (j = 0; j < num; j + 1) {
+				result = result + digit[digitClass[i]];
 			}
 
-			//Calculate the remainder
-			currVal=currVal%digitClass[i];
+			/* CALCULATE THE REMAINDER */
+			currVal = currVal % digitClass[i];
 			
 		}
 	}
